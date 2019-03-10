52c6053e8c1c799dd035b2bb24cc37795912489c
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-03-09 21:37:40 -0700

    added font awesome to project and put in logo here, no css yet

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function NotFound() {
    return (
       <div>
+         <i className="fas fa-exclamation-circle" />
          <h2>Page Not Found</h2>
       </div>
    );
